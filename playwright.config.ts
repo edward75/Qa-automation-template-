f6d03bf2ae8990a8a936671ada55bf07c195d342
edward75 <edhiland@hotmail.com>
2026-07-09 14:20:00 +0100

    Add Playwright configuration file

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+
+ testDir: './tests',
+
+ timeout: 30000,
+
+ use: {
+   headless: true,
+   browserName: 'chromium'
+ },
+
+ reporter:[
+   ['html']
+ ]
+
+});

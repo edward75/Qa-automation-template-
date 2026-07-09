45da824b60e291db7e28b24ad037f03cc97f64fe
edward75 <edhiland@hotmail.com>
2026-07-09 15:07:46 +0100

    Add ApiClient class with HTTP methods
    Implement methods for GET, POST, PUT, and DELETE requests.

--- api/ApiClient.ts
+++ api/ApiClient.ts
@@ -1,1 +1,62 @@
+import { APIRequestContext, APIResponse } from '@playwright/test';
 
+
+export class ApiClient {
+
+    constructor(
+        private request: APIRequestContext
+    ){}
+
+
+    async get(
+        url:string,
+        headers = {}
+    ):Promise<APIResponse>{
+
+        return await this.request.get(url,{
+            headers
+        });
+
+    }
+
+
+    async post(
+        url:string,
+        body:any,
+        headers={}
+    ):Promise<APIResponse>{
+
+        return await this.request.post(url,{
+            data:body,
+            headers
+        });
+
+    }
+
+
+    async put(
+        url:string,
+        body:any,
+        headers={}
+    ):Promise<APIResponse>{
+
+        return await this.request.put(url,{
+            data:body,
+            headers
+        });
+
+    }
+
+
+    async delete(
+        url:string,
+        headers={}
+    ):Promise<APIResponse>{
+
+        return await this.request.delete(url,{
+            headers
+        });
+
+    }
+
+}

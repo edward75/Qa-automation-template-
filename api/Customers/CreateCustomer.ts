922e9fb6d6f4ed773767042cb89d23aa5217b870
edward75 <edhiland@hotmail.com>
2026-08-20 12:13:11 +0100

    Implement CreateCustomer class for API requests

--- api/Customers/CreateCustomer.ts
+++ api/Customers/CreateCustomer.ts
@@ -1,2 +1,21 @@
 import { ApiClient } from '../ApiClient';
 import { Endpoints } from '../Endpoints';
+import { APIResponse } from '@playwright/test';
+
+export class CreateCustomer {
+
+    constructor(
+        private apiClient: ApiClient
+    ) {}
+
+    async execute(
+        customerData: object
+    ): Promise<APIResponse> {
+
+        return await this.apiClient.post(
+            Endpoints.customers.create,
+            customerData
+        );
+
+    }
+}
